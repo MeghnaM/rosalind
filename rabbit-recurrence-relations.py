1cdb7b79a599cb8b23ddc8ab30cd276b5dc26cae
Meghna Malhotra <mm3886@drexel.edu>
2017-06-27 11:50:55 -0700

    Recursive fibonacci

--- rabbit-recurrence-relations.py
+++ rabbit-recurrence-relations.py
@@ -3,18 +3,16 @@
 # T(n) = T(n-1) + 3T(n-2)
 # 1 1 4 7 19
 # Recursive Fibonacci
-def fib_rabbits(n, k):
+def recursive_fib(n,k):
    if (n == 1 or n == 2):
       return 1
    else:
-      return fib_rabbits(n-1, k) + 3*(fib_rabbits(n-2, k))
+      return recursive_fib(n-1, k) + 3*(recursive_fib(n-2, k))
 
 if __name__ == '__main__':
    f = open('rosalind_fib.txt', 'r')
    s = f.readline()
    f.close()
    n, k = s.split(' ')
+   print recursive_fib(int(n), int(k))
 
-   print fib_rabbits(int(n), int(k))
-
-# Dynamic Programming Fibonacci
