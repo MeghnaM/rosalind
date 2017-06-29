f69e1a0f4f7355e61cedfe0b653a1f9b9544d7ab
Meghna Malhotra <mm3886@drexel.edu>
2017-06-28 23:04:15 -0700

    In progress

--- gc-content.py
+++ gc-content.py
@@ -10,8 +10,14 @@ def gc_content(dna):
    return (gc_total/dna_total) * 100
 
 if __name__ == '__main__':
+   gc_max = 0
+
    # TODO Read lines from file, calculate GC content for all dna strings,
-   #      then print the id of the one which won, and its GC content in the next line
+   # then print the id of the one which won, and its GC content in the next line
+   with open('rosalind_gc.txt','r') as file_obj:
+      for line in file_obj:
+         if line[0] is '<':
+
    # TODO Experiment: Try to write this output to a file
    dna = 'CCACCCTCGTGGTATGGCTAGGCATTCAGGAACCGGAGAACGCTTCAGACCAGCCCGGACTGGGAACCTGCGGGCAGTAGGTGGAAT'
    # TODO Truncate floating point number to be 6 decimal places (without rounding)
