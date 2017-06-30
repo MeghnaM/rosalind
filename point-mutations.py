8e07470094288a49484e6810b3c84ffefa570174
Meghna Malhotra <mm3886@drexel.edu>
2017-06-29 23:06:15 -0700

    Setup, but not evaluated

--- point-mutations.py
+++ point-mutations.py
@@ -4,7 +4,6 @@
 def count_pt_mutations(s,t):
    if (len(s) != len(t)): return -1
    dist = 0
-
    #1 Map over both strings simultaniously, and create a 3rd list of 1's (mismatch) and 0's (match), then reduce the 3rd list by adding all elements
 
    return result
