636ab28bc13b864dda8d6200b059b41fc369fdda
Meghna Malhotra <mm3886@drexel.edu>
2017-06-30 23:08:07 -0700

    Setup

--- mendelian-inheritance.py
+++ mendelian-inheritance.py
@@ -4,4 +4,8 @@
 # hh = 0.75, hd = 1,   hr = 0.5
 # rr = 0,    rh = 0.5, rd = 1
 def mendels_first_law(k,m,n):
-   pass
+   print ""
+
+if __name__ == '__main__':
+   k, m, n = 2, 2, 2
+   mendels_first_law(k,m,n)
