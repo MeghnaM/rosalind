1ffdedc1a71661dd8824d17f1b81ca07fe0cf2f6
Meghna Malhotra <mm3886@drexel.edu>
2017-07-24 22:09:54 -0700

    Completed problem: Counting point mutations

--- point-mutations.py
+++ point-mutations.py
@@ -3,12 +3,15 @@
 # Hamming distance - The minimum number of symbol substitutions required to change one string into another of equal length
 def count_pt_mutations(s,t):
    if (len(s) != len(t)): return -1
-   dist = 0
-   #1 Map over both strings simultaniously, and create a 3rd list of 1's (mismatch) and 0's (match), then reduce the 3rd list by adding all elements
-
-   return result
+   hdist = 0
+   los = list(s)
+   lot = list(t)
+   for i in range(len(los)):
+      if los[i] != lot[i]: hdist += 1
+   return hdist
 
 if __name__ == '__main__':
-   s = 'GAGCCTACTAACGGGAT'
-   t = 'CATCGTAATGACGGCCT'
+   f = open('rosalind_hamm.txt','r')
+   s = f.readline()
+   t = f.readline()
    print count_pt_mutations(s,t)
